3778abdbf3b8b3da5575e4416ae9e1e203bc8c9e
ashtonwood1 <ashtonhughwood@outlook.com>
2024-12-06 20:41:09 +0000

    lab 11-2

--- .ccls-cache/@home@runner@chapter-11-homework-and-labs-ashtonwood1/lab11-2.cpp
+++ .ccls-cache/@home@runner@chapter-11-homework-and-labs-ashtonwood1/lab11-2.cpp
@@ -1,14 +1,50 @@
-//Lab 9-2.cpp - displays two monthly car payments
-//Created/revised by <your name> on <current date>
+//Lab 11-2.cpp 
+//Created/revised by <Ashton> on <12/6/24>
 
 #include <iostream>
 #include <cmath>
 #include <iomanip>
 using namespace std;
 
+double getAverage(double times[], int numElements);
+double getLowest(double times[], int numElements);
+
 int main()
 {
-   
+    double finishTimes[5] = {0.0};
+    double avgTime = 0.0;
+    double lowestTime = 0.0;
+
+    for (int x = 0; x < 5; x += 1)
+    {
+        cout << "Time for race " << x + 1 << ": ";
+        cin >> finishTimes[x];
+    } 
+    
+    avgTime = getAverage (finishTimes, 5);
+    lowestTime = getLowest (finishTimes, 5);
+
+    cout << fixed << setprecision(1) << endl;
+    cout << "Average 5K finish time: " << avgTime << endl;
+    cout << "Lowest 5K finish time: " << lowestTime << endl;
     
     return 0;
-}//end of main function    
+}//end of main function 
+//function definitions
+double getAverage(double times[], int numElements)
+{
+    double total = 0.0;
+    for (int x= 0; x < numElements; x+=1)
+            total +=times[x];
+
+//end for
+     return total / numElements;
+}
+double getLowest(double times[], int numElements){
+    double lowest = times[0];
+    for (int x =1; x < numElements; x +=1)
+        if (times[x] < lowest)
+            lowest = times[x];
+    return lowest;
+}
+    
